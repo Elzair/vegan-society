81b1733672189e70484223ed396c8890bc7cd63d
Elzair <elzairthesorcerer@gmail.com>
2014-04-02 14:17:24 -0400

    Bumped vegguide-crawler to 0.1.0

--- tools/vegguide-crawler/index.js
+++ tools/vegguide-crawler/index.js
@@ -1,14 +1,23 @@
 var co = require('co')
   , fs      = require('co-fs')
   , request = require('co-request')
-  , util    = require('util')
   ;
 
+function filter_vegan(arr) {
+  var results = [];
+  arr.forEach(function(element, index, array) {
+    if (element.veg_level_description === 'Vegan') {
+      results.push(element);
+    }
+  });
+  return results;
+}
 
 co(function* () {
-  var entries = [];
-  //Get method
-  for (var i=1; i<10; i++) {
+  // Write first '[' to file
+  yield fs.writeFile('locations.json', '[');
+  var separator = '\n';
+  for (var i=1; i<3000; i++) {
     var results = yield request.get({url: 'http://www.vegguide.org/region/'+i.toString(), 
       headers: {'Accept': 'application/json'}});
     var region = JSON.parse(results.body);
@@ -19,11 +28,14 @@ co(function* () {
     if (parseInt(region.entry_count, 10) > 0) {
       var locresults = yield request.get({url: 'http://www.vegguide.org/region/'+i.toString()+'/entries', 
         headers: {'Accept': 'application/json'}});
-      var locations = JSON.parse(locresults.body);
-      entries = entries.concat(locations);
+      var locations = filter_vegan(JSON.parse(locresults.body));
+      var output = JSON.stringify(locations, null, 2);
+      output = separator + output.substring(2, output.length-2);
+      separator = ',\n';
+      yield fs.appendFile('locations.json', output);
+      console.log(output);
     }
   }
-  var places = util.format('%j', entries);
-  console.log(places);
-  yield fs.writeFile('places.json', places);
+  // Write last ']' to file
+  yield fs.appendFile('locations.json', '\n]');
 })();
