87bf902d73e3d6db192d985c58e5d6d09ee952b4
Elzair <elzairthesorcerer@gmail.com>
2014-04-07 20:11:32 -0400

    Used custom svg icons in public/images/sjjb to make icons large enough for iPhone

--- public/scripts/controllers.js
+++ public/scripts/controllers.js
@@ -2,74 +2,86 @@ var mapControllers = angular.module('mapControllers', ['mapServices']);
 
 mapControllers.controller('MapCtrl', ['$scope', 'Locations',
     function($scope, Locations) {
+      // Initialize map
       var map = L.map('map');
       L.tileLayer('http://{s}.tile.cloudmade.com/3d9c4d3562fd4e699ed3589691adcb05/997/256/{z}/{x}/{y}.png', {
           attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
           maxZoom: 18
       }).addTo(map);
-      function onLocationFound(e) {
-          var radius = e.accuracy / 2;
-          L.marker(e.latlng).addTo(map)
-              .bindPopup("You are within " + radius + " meters from this point").openPopup();
-          L.circle(e.latlng, radius).addTo(map);
 
-          // Initialize all markers
-          /*var restaurantMarker = L.MakiMarkers.icon({
-            icon: 'restaurant',
-            color: '#d00',
-            size: 'l'
-          });
-          var coffeeMarker = L.MakiMarkers.icon({
-            icon: 'coffee',
-            color: '#800',
-            size: 'l'
-          });
-          var barMarker = L.MakiMarkers.icon({
-            icon: 'beer',
-            color: '#f80',
-            size: 'l'
-          });
-          var vendorMarker = L.MakiMarkers.icon({
-            icon: 'bus',
-            color: '#0c0',
-            size: 'l'
-          });
-          var groceryMarker = L.MakiMarkers.icon({
-            icon: 'grocery',
-            color: '#080',
-            size: 'l'
-          });
-          var catererMarker = L.MakiMarkers.icon({
-            icon: 'mobilephone',
-            color: '#36f',
-            size: 'l'
-          });
-          var generalMarker = L.MakiMarkers.icon({
-            icon: 'commercial',
-            color: '#c0c',
-            size: 'l'
-          });
-          var organizationMarker = L.MakiMarkers.icon({
-            icon: 'building',
-            color: '#808',
-            size: 'l'
-          });
-          var hotelMarker = L.MakiMarkers.icon({
-            icon: 'lodging',
-            color: '#00f',
-            size: 'l'
-          });
-          var otherMarker = L.MakiMarkers.icon({
-            icon: 'marker',
-            color: '#000',
-            size: 'l'
-          });*/
-          
-          // Find locations/events around user's location
-          Locations.search({lat: e.latlng.lat, lng: e.latlng.lng}).$promise.then(function(locations) {
-            locations.forEach(function(loc, index, array) {
+      // Initialize list of locations
+      $scope.locationIDs = [];
+
+      function find_nearby_locations(lat, lng) {
+        // Initialize all markers
+        var restaurantMarker = L.icon({
+          iconUrl: '/images/sjjb/restaurant.svg',
+          iconSize: [64,64],
+          popupAnchor: [0,0]
+        });
+        var coffeeMarker = L.icon({
+          iconUrl: '/images/sjjb/coffee.svg',
+          iconSize: [64,64],
+          popupAnchor: [0,0]
+        });
+        var barMarker = L.icon({
+          iconUrl: '/images/sjjb/bar.svg',
+          iconSize: [64,64],
+          popupAnchor: [0,0]
+        });
+        var vendorMarker = L.icon({
+          iconUrl: '/images/sjjb/vendor.svg',
+          iconSize: [64,64],
+          popupAnchor: [0,0]
+        });
+        var groceryMarker = L.icon({
+          iconUrl: '/images/sjjb/grocery.svg',
+          iconSize: [64,64],
+          popupAnchor: [0,0]
+        });
+        var catererMarker = L.icon({
+          iconUrl: '/images/sjjb/caterer.svg',
+          iconSize: [64,64],
+          popupAnchor: [0,0]
+        });
+        var generalMarker = L.icon({
+          iconUrl: '/images/sjjb/general.svg',
+          iconSize: [64,64],
+          popupAnchor: [0,0]
+        });
+        var organizationMarker = L.icon({
+          iconUrl: '/images/sjjb/organization.svg',
+          iconSize: [64,64],
+          popupAnchor: [0,0]
+        });
+        var hotelMarker = L.icon({
+          iconUrl: '/images/sjjb/hotel.svg',
+          iconSize: [64,64],
+          popupAnchor: [0,0]
+        });
+        var otherMarker = L.icon({
+          iconUrl: '/images/sjjb/other.svg',
+          iconSize: [64,64],
+          popupAnchor: [0,0]
+        });
+        
+        // Find locations/events around user's location
+        Locations.search({lat: lat, lng: lng}).$promise.then(function(locations) {
+          locations.forEach(function(loc, index, array) {
+            // Avoid adding the same location twice
+            var duplicate = false;
+            for (var i=0; i<$scope.locationIDs.length; i++) {
+              if ($scope.locationIDs[i] === loc._id) {
+                duplicate = true;
+                break;
+              }
+            }
+            if (!duplicate) {
+              // Push location ID to array
+              $scope.locationIDs.push(loc._id);
+
               var coords = L.latLng(loc.coordinates.coordinates[1], loc.coordinates.coordinates[0]);
-              /*var marker = null;
+              var marker = null;
               switch(loc.categories[0]) {
                 case 'Restaurant':
                   marker = restaurantMarker;
@@ -104,17 +116,37 @@ mapControllers.controller('MapCtrl', ['$scope', 'Locations',
                 default:
                   marker = otherMarker;
                   break;
-              }*/
-              L.marker(coords/*, {bounceOnAdd: true, icon: marker}*/).addTo(map)
+              }
+
+              L.marker(coords, {bounceOnAdd: true, icon: marker}).addTo(map)
                 .bindPopup(JSON.stringify(loc));
-            });
+
+            }
           });
+        });
       }
-      map.on('locationfound', onLocationFound);
-      function onLocationError(e) {
-          alert(e.message);
-      }
-      map.on('locationerror', onLocationError);
+
+      // Find more locations when map center changes
+      map.on('dragend', function(e) {
+        var center = map.getCenter();
+        find_nearby_locations(center.lat, center.lng);
+      });
+      
+      map.on('locationfound', function (e) {
+        var radius = e.accuracy / 2;
+        L.marker(e.latlng).addTo(map)
+            .bindPopup("You are within " + radius + " meters from this point").openPopup();
+        L.circle(e.latlng, radius).addTo(map);
+
+        // Search for places near user's current location
+        find_nearby_locations(e.latlng.lat, e.latlng.lng);
+      });
+      
+      map.on('locationerror', function (e) {
+        alert(e.message);
+      });
+
+      // Try to find user's current location
       map.locate({setView: true, maxZoom: 14});
     }
 ]);
