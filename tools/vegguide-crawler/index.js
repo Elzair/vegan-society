33c590fe9f930a498d641d179319cc3af614f029
Elzair <elzairthesorcerer@gmail.com>
2014-05-15 14:13:35 -0400

    cb2yield() in vegguide-crawler can now specify location of result_fn with a placeholder object {cb2yield_cb_placeholder: null}

--- tools/vegguide-crawler/index.js
+++ tools/vegguide-crawler/index.js
@@ -50,11 +50,44 @@ var include = [
 
 // Convert a function from a callback-style function to a yieldable
 function cb2yield(fn, args) {
+  // Return function suitable for use in generators
   return function(ffn) {
-    args.push(function(result) {
+    // Handle case where fn is not a valid function
+    if (!(fn instanceof Function)) {
+      throw "cb2yield requires a function as the first argument!";
+    }
+
+    args = args || [];
+    // Handle case where args is not an array
+    if (!Array.isArray(args)) {
+      throw "cb2yield requires an array as the second argument!";
+    }
+
+    // Define result function
+    var result_fn = function(result) {
       ffn(null, result);
-    });
-    fn.apply(null, args);
+    };
+
+    var new_args = [];
+    var cb_pushed = false;
+    for (var i=0; i<args.length; i++) {
+      // Push result_fn to index specified by placeholder object: {'cb2yield_cb_placeholder': null}
+      if (args[i] !== null && typeof args[i] === 'object' && args[i].hasOwnProperty('cb2yield_cb_placeholder')) {
+        new_args.push(result_fn);
+        cb_pushed = true;
+      }
+      else {
+        new_args.push(args[i]);
+      }
+    }
+
+    // If args does not contain a placeholder object, push result_fn to end of new_args
+    if (!cb_pushed) {
+      new_args.push(result_fn);
+    }
+
+    // Call fn with specified arguments
+    fn.apply(null, new_args);
   };
 }
 
