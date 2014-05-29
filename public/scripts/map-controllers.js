3795762d6ea8da5d518b400ebb936d5b2ff92db4
Elzair <elzairthesorcerer@gmail.com>
2014-05-29 12:59:35 -0400

    Fixed issue where Leaflet was not displaying Mapbox tiles and changed default marker icon sizes from 32x32 to 40x40 to remove image border

--- public/scripts/map-controllers.js
+++ public/scripts/map-controllers.js
@@ -15,84 +15,87 @@ mapControllers.controller('MapCtrl', ['$scope', 'Locations', 'leafletEvents',
     function($scope, Locations, leafletEvents) {
       L.Icon.Default.imagePath = '/images';
 
-      // Initialize map
-      $scope.defaults = {
-          tileLayers: 'https://{s}.tiles.mapbox.com/v3/elzair.hod9j49e/{z}/{x}/{y}.png'
-        , attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">MapBox</a>'
-        , maxZoom: 18
-      };
-
-      console.log(leafletEvents.getAvailableMapEvents());
-
-      // Center map on user's location
-      $scope.center = {autoDiscover: true};
-
-      // Initialize list of locations
-      $scope.locations = [];
-
       // Initialize popup template
       var template = _.template("<h2 class=\"heading <%= popup_class %>\"><%= name.en_us %></h2> <div class=\"body-content <%= popup_class %>\"><div class=\"body-text\"><p><%= short_description.en_us %></p><p id=\"distance\"><%= distance %> <%= unit %></p><a href=\"<%= hash %>/entry/<%= unique_name %>\">More info</a></div> <img class=\"popup-image\" src=\"<%= thumbnails[0] %>\" alt=\"<%= caption %>\"></div>");
 
-      // Use 64 pixels for a retina display and 32 pixels otherwise
-      $scope.width = (window.devicePixelRatio > 1) ? 64 : 32;
-      $scope.height = (window.devicePixelRatio > 1) ? 64 : 32;
+      angular.extend($scope, {
+          // Use 64 pixels for a retina display and 32 pixels otherwise
+          width: (window.devicePixelRatio > 1) ? 64 : 40
+        , height: (window.devicePixelRatio > 1) ? 64 : 40
+      });
+      angular.extend($scope, {
+          // Initialize map
+          tiles: {
+              url: 'https://{s}.tiles.mapbox.com/v3/elzair.hod9j49e/{z}/{x}/{y}.png'
+            , options: {
+                attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">MapBox</a>'
+              }
+          }
 
-      // Initialize all icons
-      $scope.icons = {
-          restaurantIcon: {
-            iconUrl: '/images/sjjb/restaurant.svg',
-            iconSize: [$scope.width,$scope.height],
-            popupAnchor: [0,-1*$scope.height/2]
-          }
-        , coffeeIcon: {
-            iconUrl: '/images/sjjb/coffee.svg',
-            iconSize: [$scope.width,$scope.height],
-            popupAnchor: [0,-1*$scope.height/2]
-          }
-        , barIcon: {
-            iconUrl: '/images/sjjb/bar.svg',
-            iconSize: [$scope.width,$scope.height],
-            popupAnchor: [0,-1*$scope.height/2]
-          }
-        , vendorIcon: {
-            iconUrl: '/images/sjjb/vendor.svg',
-            iconSize: [$scope.width,$scope.height],
-            popupAnchor: [0,-1*$scope.height/2]
-          }
-        , groceryIcon: {
-            iconUrl: '/images/sjjb/grocery.svg',
-            iconSize: [$scope.width,$scope.height],
-            popupAnchor: [0,-1*$scope.height/2]
-          }
-        , catererIcon: {
-            iconUrl: '/images/sjjb/caterer.svg',
-            iconSize: [$scope.width,$scope.height],
-            popupAnchor: [0,-1*$scope.height/2]
-          }
-        , generalIcon: {
-            iconUrl: '/images/sjjb/general.svg',
-            iconSize: [$scope.width,$scope.height],
-            popupAnchor: [0,-$scope.height/2]
-          }
-        , organizationIcon: {
-            iconUrl: '/images/sjjb/organization.svg',
-            iconSize: [$scope.width,$scope.height],
-            popupAnchor: [0,0]
-          }
-        , hotelIcon: {
-            iconUrl: '/images/sjjb/hotel.svg',
-            iconSize: [$scope.width,$scope.height],
-            popupAnchor: [0,0]
-          }
-        , otherIcon: {
-            iconUrl: '/images/sjjb/other.svg',
-            iconSize: [$scope.width,$scope.height],
-            popupAnchor: [0,-1*$scope.height/2]
-          }
-      };
+          // Center map on user's location
+        , center: {autoDiscover: true}
 
-      // Initialize set of markers
-      $scope.markers = {};
+          // Initialize list of locations
+        , locations: []
+
+          // Initialize all icons
+        , icons: {
+              restaurantIcon: {
+                iconUrl: '/images/sjjb/restaurant.svg',
+                iconSize: [$scope.width,$scope.height],
+                popupAnchor: [0,-1*$scope.height/2]
+              }
+            , coffeeIcon: {
+                iconUrl: '/images/sjjb/coffee.svg',
+                iconSize: [$scope.width,$scope.height],
+                //popupAnchor: [0,-1*$scope.height/2]
+                popupAnchor: [0, 0]
+              }
+            , barIcon: {
+                iconUrl: '/images/sjjb/bar.svg',
+                iconSize: [$scope.width,$scope.height],
+                popupAnchor: [0,-1*$scope.height/2]
+              }
+            , vendorIcon: {
+                iconUrl: '/images/sjjb/vendor.svg',
+                iconSize: [$scope.width,$scope.height],
+                popupAnchor: [0,-1*$scope.height/2]
+              }
+            , groceryIcon: {
+                iconUrl: '/images/sjjb/grocery.svg',
+                iconSize: [$scope.width,$scope.height],
+                popupAnchor: [0,-1*$scope.height/2]
+              }
+            , catererIcon: {
+                iconUrl: '/images/sjjb/caterer.svg',
+                iconSize: [$scope.width,$scope.height],
+                popupAnchor: [0,-1*$scope.height/2]
+              }
+            , generalIcon: {
+                iconUrl: '/images/sjjb/general.svg',
+                iconSize: [$scope.width,$scope.height],
+                popupAnchor: [0,-$scope.height/2]
+              }
+            , organizationIcon: {
+                iconUrl: '/images/sjjb/organization.svg',
+                iconSize: [$scope.width,$scope.height],
+                popupAnchor: [0,0]
+              }
+            , hotelIcon: {
+                iconUrl: '/images/sjjb/hotel.svg',
+                iconSize: [$scope.width,$scope.height],
+                popupAnchor: [0,0]
+              }
+            , otherIcon: {
+                iconUrl: '/images/sjjb/other.svg',
+                iconSize: [$scope.width,$scope.height],
+                popupAnchor: [0,-1*$scope.height/2]
+              }
+          }
+
+          // Initialize set of markers
+        , markers: {}
+      });
 
       function find_nearby_locations(lat, lng) {
         // Find locations/events around user's location
@@ -165,8 +168,7 @@ mapControllers.controller('MapCtrl', ['$scope', 'Locations', 'leafletEvents',
               // Push location to array
               $scope.locations.push(loc);
 
-              //var marker = L.marker(coords, {bounceOnAdd: true, icon: icon});
-              //marker.bindPopup(template(loc));
+              // Add marker to map
               $scope.markers[loc._id] = {
                   lat: loc.location.coordinates[1]
                 , lng: loc.location.coordinates[0] 
@@ -181,30 +183,32 @@ mapControllers.controller('MapCtrl', ['$scope', 'Locations', 'leafletEvents',
 
       // Find more locations when map center changes
       $scope.$on('leafletDirectiveMap.dragend', function(e, args) {
-        console.log(e);
-        console.log(args);
-        //var center = map.getCenter();
-        //find_nearby_locations(center.lat, center.lng);
+        var center = e.currentScope.center;
+        find_nearby_locations(center.lat, center.lng);
       });
       
       $scope.$on('leafletDirectiveMap.locationfound', function (e, args) {
-        console.log(e);
-        console.log(args);
-        $scope.user_location = args.leafletEvent.latlng;
-        var cradius = args.leafletEvent.accuracy / 2;
-        var cmradius = (window.devicePixelRation > 1) ? 16 : 18;
-        //L.circle(e.latlng, cradius).addTo(map);
-        //L.circleMarker(e.latlng, cmradius).addTo(map);
-
+        angular.extend($scope, {
+            user_location:  args.leafletEvent.latlng
+          , paths: {
+                circle: {
+                    type: 'circle'
+                  , radius: args.leafletEvent.accuracy / 2
+                  , latlngs: $scope.user_location
+                }
+              , circleMarker: {
+                    type: 'circleMarker'
+                  , radius: (window.devicePixelRation > 1) ? 16 : 18
+                  , latlngs: $scope.user_location
+                }
+            }
+        });
         // Search for places near user's current location
-        find_nearby_locations(args.leafletEvent.latlng.lat, args.leafletEvent.latlng.lng);
+        find_nearby_locations($scope.user_location.lat, $scope.user_location.lng);
       });
       
       $scope.$on('leafletDirectiveMap.locationerror', function (e, args) {
         alert(e.message);
       });
-
-      //// Try to find user's current location
-      //map.locate({setView: true, maxZoom: 14});
     }
 ]);
